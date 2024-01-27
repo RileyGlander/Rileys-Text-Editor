a035a1f176386bda615c343718a6641c7fc521ad
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2024-01-27 16:28:40 -0600

    added required code for database.js, install.js, src-sw.js and webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,48 +1,48 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const {InjectManifest} = require("workbox-weback-plugin")
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js"
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin',
+        template: "./index.html",
+        title: "Text Editor",
       }),
-      new MiniCssExtractPlugin(),
       new InjectManifest({
         swSrc: "./src/sw.js",
-        swDest: "service-worker.js"
+        swDest: "src-sw.js"
       }),
     
     
-      new GenerateSW(),
       new WebpackPwaManifest({
         // TODO: Create a manifest.json:
-        name: "My app",
-        short_name: "MyPWA",
-        desctiption: "Super cool app",
-        background_color: '#red',
-        crossorigin: 'use-credentials',
+        name: "Editor",
+        short_name: "Editor",
+        desctiption: "Edit text",
+        theme_color: "#red",
+        // crossorigin: "use-credentials",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
 
-          src: path.resolve('src/assets/logo.png'),
+          src: path.resolve("src/assets/logo.png"),
           sizes: [96, 128, 192, 256, 384, 512],
-          destination: [],
+          destination: path.join("assets", "icons"),
           }
 
         ]
@@ -53,19 +53,22 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          use: ["style-loader", "css-loader"],
+          // MiniCssExtractPlugin.loader
         },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
+        // {
+        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        //   type: "asset/resource",
+        // },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
+              // plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"],
+
             },
           },
         },
